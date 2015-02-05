f81e729db2382292966eb835ba73eb143b578215
Nick Spinale <spinalen@carleton.edu>
2015-02-05 11:56:49 -0600

    bendecoding.

--- Bencode.hs
+++ Bencode.hs
@@ -13,11 +13,19 @@ data BData = BDict (M.Map B.ByteString BData)
            | BString B.ByteString
            | BInteger Integer
 
+writeBen :: BData -> B.ByteString
+writeBen BDict    dict  = surround 'd' $ concatMap (\(x, y) -> append (writeFromString x) (writeBen y)) $ toList dict
+writeBen BList    list  = surround 'l' $ concatMap writeBen list
+writeBen BString  bytes = writeFromString bytes
+writeBen BInteger n     = surround 'i' $ pack $ show n
+
+writeFromString = (append $ B.pack $ b.length bytes) . cons ':'
+
+surround :: Char -> Char -> ByteString -> ByteString
+surround = (`snoc` e) . cons
+
 readBen :: B.ByteString -> Maybe BData
 readBen = undefined
-
-writeBen :: BData -> B.ByteString
-writeBen = undefined
 
 parseBData =  parseBDict
           <|> parseBList
@@ -25,15 +33,14 @@ parseBData =  parseBDict
           <|> parseBInteger
 
 parseBDict = fmap (BDict . M.fromList) $
-    P.char 'l' *> P.many1 (liftA2 (,) parseBString parseBData) <* P.char 'e'
+    P.char 'l' *> P.many1 (liftA2 (,) parseBString' parseBData) <* P.char 'e'
 
 parseBList = fmap BList $
     P.char 'l' *> P.many1 parseBData <* P.char 'e'
 
-parseBString = do
-    n <- P.decimal
-    P.char ':'
-    BString <$> P.take n
+parseBString = BString <$> parseBString'
+
+parseBString' = (P.decimal <* P.char ':') >>= P.take
 
 parseBInteger = fmap BInteger $
     P.char 'i' *> P.decimal <* P.char 'e'
