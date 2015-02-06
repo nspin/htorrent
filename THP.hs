af7884e7a2bfc526c25be7af4988569240da8582
Nick Spinale <spinalen@carleton.edu>
2015-02-05 19:51:07 -0600

    draft of thp complete

--- THP.hs
+++ THP.hs
@@ -1,12 +1,16 @@
 module THP ( THP_Req(..)
            , THP_Event(..)
+           , contact
            ) where
 
 import           Data.Bits
 import           Data.Char
 import           Data.Word
 import           Data.Digest.SHA1
+
 import qualified Data.ByteString.Char8 as B
+import qualified Data.ByteString as B2
+
 import           Network.HTTP.Base
 import           Network.HTTP
 import           Network.URI
@@ -22,18 +26,25 @@ data THP_Req = THP_Req { info_hash  :: Word160
 
 data THP_Event = Started | Stopped | Completed
 
-test = case makeReq (B.pack 
 
-mkReq :: B.ByteString -> THP_Req -> Maybe (Request String)
-mkReq base thp = do
-    theuri <- parseURI uri
-    return Request { rqURI     = theuri
-                   , rqMethod  = GET
-                   , rqHeaders = []
-                   , rqBody    = []
-                   }
+contact :: B.ByteString -> THP_Req -> IO (Maybe String)
+contact base thp = case req of
+    Nothing -> return Nothing
+    Just req -> do
+        resp <- simpleHTTP req
+        return $ case resp of
+            Left _ -> Nothing
+            Right (Response {rspBody = body}) -> Just body
   where
     uri = B.unpack base ++ "?" ++ urlEncodeVars (queries thp)
+    req = do
+        theuri <- parseURI uri
+        return Request { rqURI     = theuri
+                       , rqMethod  = GET
+                       , rqHeaders = []
+                       , rqBody    = []
+                       }
+
 
 shw Started   = "started"
 shw Stopped   = "stopped"
@@ -41,13 +52,13 @@ shw Completed = "completed"
 
 queries :: THP_Req -> [(String, String)]
 queries THP_Req { info_hash  = info_hashV
-        , peer_id    = peer_idV
-        , xport      = portV
-        , uploaded   = uploadedV
-        , downloaded = downloadedV
-        , left       = leftV
-        , event      = eventV
-        }
+                , peer_id    = peer_idV
+                , xport      = portV
+                , uploaded   = uploadedV
+                , downloaded = downloadedV
+                , left       = leftV
+                , event      = eventV
+                }
         = [ ("info_hash"  , from160 info_hashV)
           , ("peer_id"    , from160 peer_idV)
           , ("port"       , show portV)
@@ -58,9 +69,12 @@ queries THP_Req { info_hash  = info_hashV
                of   Just e  -> [("event", shw e)]
                     Nothing -> []
 
--- There's gotta be a better way to do this...
+-- Hoping that show escapes for us
 from160 :: Word160 -> String
-from160 (Word160 a b c d e) = map (chr. fromIntegral) $ concatMap chunkify [a, b, c, d, e]
+from160 (Word160 a b c d e) = show
+                            . B2.pack
+                            . map fromIntegral
+                            $ concatMap chunkify [a, b, c, d, e]
 
 chunkify :: Word32 -> [Word32]
 chunkify x = [ shiftR x 24
@@ -68,3 +82,12 @@ chunkify x = [ shiftR x 24
              , shiftR x  8
              , x
              ]
+
+-- -- There's gotta be a better way to do this...
+-- from160 :: Word160 -> String
+-- from160 (Word160 a b c d e) = mapM escape
+--                             $ map (chr. fromIntegral)
+--                             $ concatMap chunkify [a, b, c, d, e]
+
+-- escape :: Char -> String
+-- escape c = if or $ map ($ c) [isAsciiUpper, isAsciiLower, isDigit, (`elem` ".,
--- Testing.hs
+++ Testing.hs
@@ -2,7 +2,9 @@ module Testing where
 
 import           Bencode
 import           Torrent
+import           THP
 import           Control.Monad
+import           Data.Digest.SHA1
 import qualified Data.ByteString.Char8 as B
 
 test :: IO ()
@@ -12,3 +14,22 @@ test = do
 
 ifile = B.readFile "/home/nick/stuff/[kickass.so]nicki.minaj.pinkprint.deluxe.mp3.2014.torrent"
 -- thefile = return $ B.pack $ "d8:announcekk
+
+test2 :: IO()
+test2 = do
+    file <- ifile
+    let Just Torrent { announce = ann
+                , fileStuff = stuff
+                , infoHash = inf
+                } = torrentize file
+        Right ints = stuff
+        thp = THP_Req { info_hash  = inf
+                      , peer_id    = Word160 32 32 31 43 23
+                      , xport      = 542
+                      , uploaded   = 0
+                      , downloaded = 0
+                      , left       = fromIntegral $ sum ints
+                      , event      = Just Started
+                      }
+    resp <- contact ann thp
+    print resp
