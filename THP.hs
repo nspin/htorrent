10e901ec14a9410cd3734b9aa247b0727a50673a
Nick Spinale <spinalen@carleton.edu>
2015-02-06 00:12:16 -0600

    correct responses from trackers, but super hacky

--- THP.hs
+++ THP.hs
@@ -1,10 +1,13 @@
-module THP ( THP_Req(..)
-           , THP_Event(..)
-           , contact
+module THP ( contact
            ) where
 
-import  Data.ByteString.Base16
-import  Data.List
+import Torrent
+import Bencode
+
+import Data.Maybe
+import Numeric
+import Control.Monad
+import           Data.List
 import           Data.Bits
 import           Data.Char
 import           Data.Word
@@ -17,68 +20,71 @@ import           Network.HTTP.Base
 import           Network.HTTP
 import           Network.URI
 
-data THP_Req = THP_Req { info_hash  :: Word160
-                       , peer_id    :: Word160
-                       , rport      :: Word
-                       , uploaded   :: Word
+test :: IO ()
+test = do
+    file <- B.readFile "/home/nick/stuff/slackware-14.1-install-dvd.torrent"
+    let t = torrentize file
+    case t of
+        Nothing -> print "nooooooooooooooo"
+        Just Torrent { announce  = announce'
+                     , pieceLen  = pieceLen'
+                     , fileStuff = Right fileStuf'
+                     , pieces    = pieces'
+                     , infoHash  = infoHash'
+                     } -> contact
+                           (C.unpack announce')
+                           infoHash'
+                           (TInfo {peer_id = Word160 432 432 12 35 342, rport = 7000}) 
+                           (TStatus {uploaded = 0, downloaded = 0, left = negate (sum fileStuf')})
+                           (Just Started)
+            
+
+data TInfo = TInfo { peer_id :: Word160
+                   , rport   :: Word
+                   }
+
+data TStatus = TStatus { uploaded   :: Word
                        , downloaded :: Word
-                       , left       :: Word
-                       , event      :: Maybe THP_Event
+                       , left       :: Int
                        }
 
-data THP_Event = Started | Stopped | Completed
+data TEvent = Started | Stopped | Completed
 
-instance Show THP_Event where
-    show Started   = "started"
-    show Stopped   = "stopped"
-    show Completed = "completed"
+encodeEvent :: TEvent -> String
+encodeEvent Started   = "started"
+encodeEvent Stopped   = "stopped"
+encodeEvent Completed = "completed"
 
-contact :: String -> THP_Req -> IO (Maybe String)
-contact base thp = case req of
-    Nothing -> return Nothing
-    Just req -> do
-        print $ C.unpack $ encode $ C.pack $ from160 info
-        print uri
-        resp <- simpleHTTP req
-        return $ case resp of
-            Left _ -> Nothing
-            Right (Response {rspBody = body}) -> Just body
-  where
-    THP_Req {info_hash = info} = thp
-    uri = base ++ '?' : intercalate "&" [ x ++ "=" ++ y | (x, y) <- queries thp ]
-    req = do
-        theuri <- parseURI uri
-        return Request { rqURI     = theuri
-                       , rqMethod  = GET
-                       , rqHeaders = []
-                       , rqBody    = []
-                       }
-
-queries :: THP_Req -> [(String, String)]
-queries THP_Req { info_hash  = info_hash'
-                , peer_id    = peer_id'
-                , rport      = port'
-                , uploaded   = uploaded'
+contact :: String -> Word160 -> TInfo -> TStatus -> Maybe TEvent -> IO ()
+contact tracker
+        info
+        TInfo { peer_id = peer_id'
+              , rport   = rport'
+              }
+        TStatus { uploaded   = uploaded'
                 , downloaded = downloaded'
                 , left       = left'
-                , event      = event'
                 }
-        = [ ("info_hash"  , urlEncode $ from160 info_hash')
-          , ("peer_id"    , urlEncode $ from160 peer_id')
-          , ("port"       , show port')
-          , ("uploaded"   , show uploaded')
-          , ("downloaded" , show downloaded')
-          , ("left"       , show left')
-          ] ++ case event'
-               of   Just e  -> [("event", show e)]
-                    Nothing -> []
+        event
+  = do
+    print uri
+    resp <- simpleHTTP $ getRequest uri
+    print resp
+  where uri = tracker ++ "?" ++ intercalate "&"
+            [ "info_hash=" ++ encode160 info
+            , "peer_id="   ++ encode160 peer_id'
+            , "port=" ++ show rport'
+            , "uploaded=" ++ show uploaded'
+            , "downloaded=" ++ show downloaded'
+            , "left=" ++ show left'
+            , fromMaybe "" (liftM (("event=" ++) . encodeEvent) event)
+            ]
 
-from160 :: Word160 -> String
-from160 (Word160 a b c d e) = map (chr . fromIntegral)
-                            $ concatMap chunkify [a, b, c, d, e]
+encode160 :: Word160 -> String
+encode160 (Word160 a b c d e) = map toUpper ([a, b, c, d, e] >>= chunk32 >>= (\y -> let x = showHex y "" in '%' : (if length x == 1 then '0':x else x)))
 
-chunkify :: Word32 -> [Word8]
-chunkify x = map fromIntegral [ shiftR x 24
+chunk32 :: Word32 -> [Word8]
+chunk32 x = map fromIntegral [ shiftR x 24
                              , shiftR x 16
                              , shiftR x  8
                              , x
