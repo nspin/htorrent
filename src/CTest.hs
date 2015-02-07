6fb8a34ad322fcde2368d486e8713c46ba378898
Nick Spinale <spinalen@carleton.edu>
2015-02-06 23:24:05 -0600

    see last commit

--- src/CTest.hs
+++ src/CTest.hs
@@ -2,6 +2,7 @@ module CTest where
 
 import Network.Simple.TCP
 
+main :: IO ()
 main = connect "137.22.184.35" "6881" $ \(sock, addr) -> do
     print sock
     print addr
--- src/STest.hs
+++ src/STest.hs
@@ -2,6 +2,7 @@ module STest where
 
 import Network.Simple.TCP
 
+main :: IO ()
 main = serve (Host "127.0.0.1") "6881" $ \(sock, addr) -> do
     print sock
     print addr
