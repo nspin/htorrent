cc27f864025c70dae45ab3dea05ca8a6abc3e386
Nick Spinale <spinalen@carleton.edu>
2015-02-07 10:36:36 -0600

    working on internals

--- src/Curtis/Core.hs
+++ src/Curtis/Core.hs
@@ -1,15 +1,28 @@
-module Curtis.Core ( download
-                   ) where
+module Curtis.Core where
 
 import Data.Map
+import Control.Concurrent
+import Control.Concurrent.MVar
+import Data.Lens
 
-data Tate = Tate { peers :: [Peer]
-                 , file  :: 
+type T = ReaderT Info IO
+type P = ReaderT Book IO
+
+-- infohash then pid
+data Info = (B.ByteString, B.ByteString)
+
+data Book = Book { peerM :: MVar peer
+                 , msgsM :: MVar (Seq Message)
+                 , infoT :: Info
+                 }
+
+data Peer = Peer { id :: PeerID
+                 , relation :: Relation
+                 , status :: Map Integer Bool
+                 , sock :: Socket
                  }
 
 type PeerID = (String, String) -- addr, port
-
-data Peer = Peer PeerID Relation (Map Integer Bool)
 
 data Relation = Relation { choked      :: Bool
                          , choking     :: Bool
@@ -17,7 +30,46 @@ data Relation = Relation { choked      :: Bool
                          , interesting :: Bool
                          }
 
-download :: FilePath -- where to store pieces
-         -> MVar [PeerID] -- up-to-date peer reports from tracker or dht
-         -> MVar [Peer]
-         -> IO
+data Message = Keepalive
+             | Choke
+             | Unchoke
+             | Intersted
+             | Bored
+             | Have Integer
+             | Bitfield Map Integer Bool
+             | Piece Integer Integer B.ByteString
+             | Request Integer Integer Integer
+             | Cancel Integer Integer Integer
+             deriving Show
+
+pstr :: B.ByteString
+pstr = C.pack "BitTorrent protocol"
+
+handhead :: B.ByteString
+handhead = concat [ B.pack 19
+                  , pstr
+                  , B.replicate 8 0
+                  ]
+
+meet :: PeerID -> P ()
+meet di@(addr, prt) = do
+    (ihash, mid) <- asks infoT
+    peers <- readMVar mvar
+    unless ((==) id' . id `any` peers)
+     . connect addr prt
+     $ \(sock, _) -> do
+        forkIO $ send sock $ concat [handhead, ihash, mid]
+        forkIO $ do
+            resp <- recv sock 1337
+            case resp of
+                Nothing -> return ()
+                Just bytes -> do
+                    -- todo test things like infohash and protocol
+                    forkIO $ forever $ do
+                        msgs <- asks msgsM
+                        liftIO $ do
+                            com <- readMVar msgs
+                            doCom com
+                    forever $ liftIO $ do
+                        req <- recv sock 133337
+                        parseAndDoReq
