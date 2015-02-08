f2c05c0d012b371e9aa0a2415ea2b447c95663e3
Nick Spinale <spinalen@carleton.edu>
2015-02-07 22:09:24 -0600

    orgnanization

--- src/Curtis/Parsers/Bencode.hs
+++ src/Curtis/Parsers/Bencode.hs
@@ -6,7 +6,6 @@ module Curtis.Parsers.Bencode
     , getList
     , getDict
     , bookup
-    , bencode
     , hashify
     ) where
 
@@ -40,6 +39,7 @@ getDict (BDict v) = Just v
 getDict _ = Nothing
 
 -- TODO improve (with attoparsec)
+-- Also, this is purely for convenience
 bookup :: String -> [(C.ByteString, a)] -> Maybe a
 bookup skey = lookup (C.pack skey)
 
@@ -52,22 +52,6 @@ parseBen =  BString <$> parseString
 parseMid start middle = char start *> middle <* char 'e'
 
 parseString = decimal <* char ':' >>= P.take
-
--- Bencodes a BValue into a wing
-bencode :: BValue -> C.ByteString
-bencode (BString bytes) = writeBytes bytes
-bencode (BInt    int  ) = surround 'i' . C.pack $ show int
-bencode (BList   list ) = surround 'l' . C.concat $ map bencode list
-bencode (BDict   dict ) = surround 'd' . C.concat $ map writePair dict
-
-surround :: Char -> C.ByteString -> C.ByteString
-surround start = (start `C.cons`) . (`C.snoc` 'e')
-
-writePair :: (C.ByteString, BValue) -> C.ByteString
-writePair (key, value) = writeBytes key `C.append` bencode value
-
-writeBytes :: C.ByteString -> C.ByteString
-writeBytes bytes = (C.pack . show $ C.length bytes) `C.append` C.cons ':' bytes
 
 -- Extract raw bytestring of info key (if it exists), for use in calculating infohash
 hashify :: B.ByteString -> Maybe B.ByteString
--- src/Curtis/Types.hs
+++ src/Curtis/Types.hs
@@ -1,21 +1,21 @@
-module Curis.Types (
+module Curis.Types where
 
 import Data.ByteString
 import Data.Map
 
+-- Our monad stack
 type T = ReaderT Download (StateT (MVar Tate) IO)
-
 
 -- progress is a map from piece number to pairs of start-end bites of needed blocks
 data Tate = Tate { progress :: Map Integer [(Integer, Integer)]
                  , --peers    :: [MVar Peer]
-                 }
+                 } deriving Show
 
 data Progress = Progress { pieces  :: Map Integer Bool
                          , current :: Integer
                          , cprog   :: [(Int, Int)]
                          , tstat   :: TStatus
-                         }
+                         } deriving Show
 
 -- infohash, pid, then pieces
 data Download = Download { tracker  :: String
@@ -23,7 +23,7 @@ data Download = Download { tracker  :: String
                          , metahash :: B.ByteString
                          , myport   :: Integer
                          , phashes  :: [B.ByteString]
-                         }
+                         } deriving Show
 
 data Peer = Peer { id       :: PeerID -- necessary? (yes for tracker thread decisions)
                  , sock     :: Socket -- necessary?
@@ -32,7 +32,7 @@ data Peer = Peer { id       :: PeerID -- necessary? (yes for tracker thread deci
                  , up       :: Word
                  , down     :: Word
                  , cchan    :: Command
-                 }
+                 } deriving Show
 
 -- addr, port
 type PeerID = (String, String)
@@ -41,15 +41,9 @@ data Relation = Relation { choked      :: Bool
                          , choking     :: Bool
                          , inerested   :: Bool
                          , interesting :: Bool
-                         }
+                         } deriving Show
 
--- A bencoded value
-data BValue = BString B.ByteString
-            | BInt Integer
-            | BList [BValue]
-            | BDict [(B.ByteString, BValue)]
-            deriving Show
-
+-- Peer Wire Protocol types
 
 data Handshake = Handshake String B.ByteString B.ByteString
 
@@ -65,71 +59,59 @@ data Message = Keepalive
              | Cancel Int Int Int
              deriving Show
 
+
+-- A bencoded value
+data BValue = BString B.ByteString
+            | BInt Integer
+            | BList [BValue]
+            | BDict [(B.ByteString, BValue)]
+            deriving Show
+
+-- Communication with tracker
+
 data TRequest = TRequest { tracker    :: String
                          , info_hash  :: B.ByteString
                          , peer_id    :: B.ByteString
                          , pport      :: Word
                          , status     :: TStatus
-                         , compact    :: Bool
-                         , no_peer_id :: Bool
                          , event      :: Maybe TEvent
-                         , ip         :: Maybe String
-                         , numwant    :: Maybe Word
-                         , key        :: Maybe B.ByteString
-                         , trackerid  :: Maybe String
-                         }
-  deriving Show
+                         , ip         :: String
+                         , key        :: B.ByteString
+                         , trackerid  :: String
+                         } deriving Show
   
   
-data TStatus = TStatus { uploaded   :: Integer
-                       , downloaded :: Integer
-                       , left       :: Integer
-                       }
-  deriving Show
+data OurStatus = OurStatus { uploaded   :: Integer
+                           , downloaded :: Integer
+                           , left       :: Integer
+                           } deriving Show
 
-data TEvent = Started | Stopped | Completed
-  deriving Show
+data OurEvent = Started | Stopped | Completed deriving Show
 
+-- All of the information in(/about) a torrent file that curtis is,
+-- at this point, capable of caring about.
 
 data MetaInfo = MetaInfo Torrent B.ByteString deriving Show
 
--- All of the information in a torren file that curtis is,
--- at this point, capable of caring about. Curtis will grow,
--- though, so this will expand at some point.
 data Torrent = Torrent
     { announce      :: String
     , announce_list :: Maybe [String]
     , comment       :: Maybe String
     , created_by    :: Maybe String
     , creation_date :: Maybe Integer
+    , encoding      :: Maybe String
     , infoStuff     :: InfoStuff
-    }
-  deriving Show
+    } deriving Show
 
-data InfoStuff = InfoStuff
+data Info = Info
     { piece_length :: Integer
     , pieces       :: [B.ByteString]
     , private      :: Bool
-    , fileStuff    :: Either OneFile ManyFiles
-    }
-  deriving Show
+    , files        :: Either (FileInfo String) (String, FileInfo [String])
+    } deriving Show
 
-data OneFile = OneFile
-    { nameO   :: String
-    , lengthO :: Integer
-    , md5sumO :: Maybe B.ByteString
-    }
-  deriving Show
-
-data ManyFiles = ManyFiles
-    { nameM  :: String -- semanticaly different from OneFile name, so seperate.
-    , filesM :: [ManyFile]
-    }
-  deriving Show
-
-data ManyFile = ManyFile
-    { pathM  :: [String]
-    , lengthM :: Integer
-    , md5sumM :: Maybe B.ByteString -- (Word64, Word64)
-    }
-  deriving Show
+data FileInfo a = FileInfo
+    { name   :: a
+    , length :: Integer
+    , md5sum :: Maybe B.ByteString
+    } deriving Show
